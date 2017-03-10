9d841714ba1de468c491ea8faf71a85484087b1a
sivan <sivblurg@hotmail.com>
2017-03-10 08:25:52 +0200

    initial commit

---
+++ app/src/main/java/com/battery/percent/twenty/thirdjam/MusicianActivity.java
@@ -0,0 +1,13 @@
+package com.battery.percent.twenty.thirdjam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MusicianActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_musician);
+    }
+}
